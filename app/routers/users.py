b295d95b465e738a7ac629c4222d61097942a8e7
Georgi Iliev <georgi.iliev533@outlook.com>
2024-03-15 23:44:35 +0200

    Added endpoint for getting user data

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,3 +1,18 @@
-from fastapi import APIRouter
+from typing import Annotated
+from datetime import datetime, timedelta, timezone
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from app import crud, models, schemas
+from app.dependencies import get_db
+from app.auth import get_current_active_user
 
 users = APIRouter()
+
+
+@users.get("/me", response_model=schemas.User)
+async def read_users_me(
+    current_user: Annotated[schemas.User, Depends(get_current_active_user)]
+):
+    return current_user
