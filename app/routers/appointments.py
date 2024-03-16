47fd78fc1b68b4c4541648d25dd8ced923cdcadd
Georgi Iliev <georgi.iliev533@outlook.com>
2024-03-16 16:38:50 +0200

    Add authentication for getting appointments

--- app/routers/appointments.py
+++ app/routers/appointments.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 
 from app import crud, models, schemas
 from app.dependencies import get_db
+from app.auth import get_current_active_user
 
 appointments = APIRouter()
 
@@ -20,6 +21,7 @@ def create_appointment(appointment: schemas.AppointmentCreate, db: Session = Dep
 @appointments.get('/get', response_model=list[schemas.Appointment])
 def get_appointments_between(
     appointments_request: schemas.AppointmentsBetweenRequest = Depends(),
+    current_user: models.User = Depends(get_current_active_user),
     db: Session = Depends(get_db)
 ):
-    return crud.get_appointments_between(db, appointments_request)
+    return crud.get_appointments_between(db, current_user.account_uuid, appointments_request)
